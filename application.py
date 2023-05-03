851585564ba44eaf2bcfe8eec3d0708fee56b120
DanielZh <haveacar.zhovnir@gmail.com>
2023-05-03 13:38:55 +0300

    start flask

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    application.run(port=5002, debug=True)
