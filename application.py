f7d4fad06087998f216e8cca36ef7b02172077c1
DanielZh <haveacar.zhovnir@gmail.com>
2023-05-09 14:02:14 +0300

    added controls.py

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from controls import *
+
 
 application = Flask(__name__)
 
 
-@application.route('/')
-@application.route('/home')
+departure_date, return_date= get_data_travel()
+
+@application.route('/', methods=['GET', 'POST'])
+@application.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data_departure=departure_date, data_return=return_date)
 
 @application.route('/about')
 def about():
---
+++ controls.py
@@ -0,0 +1,28 @@
+import datetime
+
+def get_data_travel(number_weeks=2) -> tuple:
+    """
+    func to generate departing date and return with Israel preferences
+    :return: tuple of strings
+    """
+
+    current_date = datetime.date.today()
+
+    # Add two weeks to the current date
+    two_weeks = datetime.timedelta(weeks=number_weeks)
+    future_date = current_date + two_weeks
+
+    # Loop until we find a Thursday day
+    while future_date.weekday() != 3:
+        future_date += datetime.timedelta(days=1)
+
+    # create departure string
+    one_day = datetime.timedelta(days=1)
+    data_departure = f"{future_date}_{future_date + one_day}"
+
+    # create return string
+    sunday_return = future_date + datetime.timedelta(days=3)
+    two_days = datetime.timedelta(days=2)
+    data_return = f"{sunday_return}_{sunday_return + two_days}"
+
+    return data_departure, data_return
