fd5de73734864be0a37b854cfa71de2db5578459
DanielZh <haveacar.zhovnir@gmail.com>
2023-05-10 16:17:02 +0300

    added currency def

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from controls import *
 
 
@@ -6,6 +6,13 @@ application = Flask(__name__)
 
 
 departure_date, return_date= get_data_travel()
+
+conversion_rates = Currency()
+
+# get current data rates
+data = conversion_rates.currency_convector().get("date")
+# list comprehension of keys rates
+currencies = [key for key in conversion_rates.currency_convector().get("rates")]
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/home', methods=['GET', 'POST'])
@@ -16,7 +23,7 @@ def home():
 def search():
     return render_template('search.html')
 
-@application.route('/currency')
+@application.route('/currency', methods=['GET', 'POST'])
 def currency():
     if request.method == 'POST':
 
@@ -28,10 +35,10 @@ def currency():
                           conversion_rates.currency_convector().get("rates")[from_currency]
         converted_amount = round(amount * conversion_rate, 2)
         # return template rates
-        return render_template('index.html', data=data, amount=amount, from_currency=from_currency + " =",
+        return render_template('currency.html', data=data, amount=amount, from_currency=from_currency + " =",
                                converted_amount=converted_amount, to_currency=to_currency, currencies=currencies)
     else:
-        return render_template('index.html', data=data, currencies=currencies)
+        return render_template('currency.html', data=data, currencies=currencies)
 
 
 if __name__ == '__main__':
--- controls.py
+++ controls.py
@@ -1,4 +1,16 @@
 import datetime
+import os
+
+import requests as requests
+from flask import json
+
+# api key and request url
+API_KEY = "c66xOBOerxjgjCqRfbT3MzvIIqMoDm6e"
+REQUEST_URL = "https://api.apilayer.com/fixer/latest?base=USD"
+
+# path
+CURRENT_PATH = os.path.dirname(__file__)
+CURRENT_PATCH_JASON = os.path.join(CURRENT_PATH, "static")
 
 def get_data_travel(number_weeks=3) -> tuple:
     """
@@ -24,5 +36,62 @@ def get_data_travel(number_weeks=3) -> tuple:
     data_return = f"{sunday_return}_{sunday_return + datetime.timedelta(days=3)}"
 
     return data_departure, data_return
+
+class Currency:
+    '''Currency convertor'''
+
+    def __init__(self):
+        pass
+
+    def currency_convector(self):
+
+        def receive_data():
+            """
+            Receive_data func check
+            :return:  dict(collected_rates) or False
+            """
+            try:
+                # receive data
+                response_data = requests.get(REQUEST_URL, headers={"UserAgent": "XY", "apikey": API_KEY})
+                collected_rates = json.loads(response_data.text)
+                if collected_rates.get("success") == True: return collected_rates
+
+            except:
+                return False
+
+        def reload_rates() -> dict:
+            """
+            Func Reload_data checks date from file
+            if date!= date.now try upload
+            :return: dict (rates), str(date)
+            """
+            time_now = datetime.datetime.now().strftime("%Y-%m-%d")
+            # open rates from file
+            with open(os.path.join(CURRENT_PATCH_JASON, "data_rates.json")) as f:
+                rates_from_file = json.load(f)
+            rates = rates_from_file
+
+            # check from file current date
+            if rates_from_file.get("date") != time_now:
+                print("Uploading Data")
+                rates = receive_data()
+
+                # cannot receive data
+                if rates == False:
+                    print("Cannot update")
+                    rates = rates_from_file
+                else:
+                    print("Uploading successful")
+                    # create file
+                    with open(os.path.join(CURRENT_PATCH_JASON, "data_rates.json"), "w") as f:
+                        json.dump(rates, f, indent=4)
+                        pass
+            return rates
+
+        all_rates = reload_rates()
+
+        return all_rates
+
+
 
 
