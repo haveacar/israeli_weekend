f02c0df954fdca043faf20c90c6b5ccc42074eb0
DanielZh <haveacar.zhovnir@gmail.com>
2023-07-14 15:27:03 +0300

    fixed views.py

--- models.py
+++ models.py
@@ -1,14 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
-
-
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
+    registered_on = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __init__(self,email, password):
         self.email = email
